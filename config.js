8dcd8b4f7c8127d650fadf785f7c7f9bf5158e79
Mdex <76643605+mdexSwap@users.noreply.github.com>
2021-01-11 21:23:58 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.__CONFIG__ = {
+  // provider: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+  // netid: 97,
+  debug: process.env.NODE_ENV === 'production' ? false : true,
+  networkID: 256
+}
